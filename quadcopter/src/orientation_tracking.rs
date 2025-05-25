c4c2d824412413ed4ca691f300bb26eddeed7652
Andrew Wock <ajwock@gmail.com>
2025-05-25 15:20:57 -0400

    First great flight: Re-added complementary filter with accel hardware lowpassed
    Signed-off-by: Andrew Wock <ajwock@gmail.com>

--- quadcopter/src/orientation_tracking.rs
+++ quadcopter/src/orientation_tracking.rs
@@ -8,11 +8,16 @@ use crate::motion_data::{MotionData, FixedMotionData, UnityFixed16, TiltData, Ra
 use az::Cast;
 use crate::debug_println;
 use esp_println::println;
+use fixed_macro::fixed;
+use fixed_trigonometry::atan::atan2;
+use fixed::types::I12F20;
 
 // This data structure represents integrated and fused IMU data to estimate
 // orientation, speed, and position.
 pub struct OrientationTracker<M: Imu> {
     pub orientation: [DegreeFixed32; 3],
+    pub fused_orientation: [DegreeFixed32; 3],
+    pub accel_tilt: [DegreeFixed32; 2],
     pub speed: [DegreeFixed32; 3],
     pub position: [DegreeFixed32; 3],
     pub last_gyro_timestamp: u16,
@@ -20,22 +25,31 @@ pub struct OrientationTracker<M: Imu> {
     pub imuctl: ImuController<M>,
 }
 
-//const DEGREE_SCALING_FACTOR: i32 = 64000;
-const DEGREE_SCALING_FACTOR: i32 = 640;
-const HALVED_DEGREE_SCALING_FACTOR: i32 = DEGREE_SCALING_FACTOR / 2;
+const ACCEL_SCALING_FACTOR: I12F20 = fixed!(0.0011962891: I12F20);
+pub fn reading_to_accel_ms2(reading: i16) -> DegreeFixed32 {
+    (reading as i32) * ACCEL_SCALING_FACTOR
+}
+
+const DEGREE_SCALING_FACTOR: I12F20 = fixed!(0.0000061037019: I12F20);
 pub fn reading_to_dps_32(reading: i16) -> DegreeFixed32 {
-    DegreeFixed32::from_bits(reading as i32) * DEGREE_SCALING_FACTOR
+    (reading as i32) * DEGREE_SCALING_FACTOR
 }
 
 // Why this?  Saves us a separate muliply or division later on
+const HALVED_DEGREE_SCALING_FACTOR: I12F20 = fixed!(0.0000030518509: I12F20);
 pub fn halved_reading_to_dps_32(reading: i16) -> DegreeFixed32 {
-    DegreeFixed32::from_bits(reading as i32) * HALVED_DEGREE_SCALING_FACTOR
+    (reading as i32) * HALVED_DEGREE_SCALING_FACTOR
 }
 
-const TIME_SCALE_FACTOR: i32 = 1_099_512;
-pub fn timestamp_diff_to_seconds(timestamp: u16) -> DegreeFixed32 {
-    DegreeFixed32::from_bits(timestamp as i32) * DegreeFixed32::from_bits(TIME_SCALE_FACTOR)
+// .000001 * 100
+const TIME_SCALE_FACTOR: I12F20 = fixed!(0.0001: I12F20);
+pub fn timestamp_diff_to_seconds(timestamp: u16) -> I12F20 {
+    // Casting plus conversion from micros to seconds
+    // I'm not entirely sure but I think this only works for U20 specifically
+    (timestamp as i32) * TIME_SCALE_FACTOR
 }
+
+const FRAC_180_PI: DegreeFixed32 = fixed!(57.29577: I12F20);
 
 const DEGREES_180: DegreeFixed32 = DegreeFixed32::from_bits(188743680);
 const DEGREES_NEG_180: DegreeFixed32 = DegreeFixed32::from_bits(-188743680);
@@ -55,6 +69,8 @@ impl<M: Imu> OrientationTracker<M> {
     pub fn new(imuctl: ImuController<M>) -> Self {
         Self {
             orientation: [DegreeFixed32::from_bits(0); 3],
+            fused_orientation: [DegreeFixed32::from_bits(0); 3],
+            accel_tilt: Default::default(),
             speed: [DegreeFixed32::from_bits(0); 3],
             position: [DegreeFixed32::from_bits(0); 3],
             last_gyro_timestamp: 0,
@@ -67,6 +83,15 @@ impl<M: Imu> OrientationTracker<M> {
         self.orientation
     }
 
+    pub fn att_accel(z_accel: DegreeFixed32, proj_accel: DegreeFixed32) -> DegreeFixed32 {
+//        atan2(z_accel, proj_accel)) * FRAC_180_PI
+        todo!()
+    }
+
+    const COMPLEMENTARY_ALPHA: DegreeFixed32 = fixed!(0.996: I12F20);
+    pub fn complementary_filter(gyro_degrees: I12F20, accel_degrees: I12F20) -> I12F20 {
+        Self::COMPLEMENTARY_ALPHA * gyro_degrees + (I12F20::ONE - Self::COMPLEMENTARY_ALPHA) * accel_degrees
+    }
     pub async fn track(&mut self) {
         let mut err_count = 0;
         loop {
@@ -85,8 +110,18 @@ impl<M: Imu> OrientationTracker<M> {
                 }
             };
             let timestamp = msg.timestamp;
-            let _accel_data = msg.accel_data;
+            let accel_data = msg.accel_data;
             let gyro_data = msg.gyro_data;
+
+            let [acc_x, acc_y, acc_z] = accel_data.map(|elt| reading_to_accel_ms2(elt) * fixed!(0.1: I12F20));
+            // Oh... I'm now aware of so much wrong I've been doing to myself...
+            // Maybe I need to explicitly use the terms pitch and roll.  TODO
+            //
+            let att_acc_y = atan2(-acc_x, (acc_y*acc_y + acc_z*acc_z).sqrt()) * FRAC_180_PI;
+            //let att_acc_y = -Self::att_accel(acc_z, acc_x);
+            let att_acc_x = atan2(acc_y, (acc_x*acc_x + acc_z*acc_z).sqrt()) * FRAC_180_PI;
+//            let att_acc_x = Self::att_accel(acc_z, acc_y);
+            self.accel_tilt = [att_acc_x, att_acc_y];
 /*            if gyro_data[0] as u16 == 0xffff || gyro_data[1] as u16 == 0xffff || gyro_data[2] as u16 == 0xffff {
                 panic!("Got erronious gyro data value");
             }*/
@@ -113,9 +148,13 @@ impl<M: Imu> OrientationTracker<M> {
                 let delta_orientation = avg_derivative * time_step;
                 debug_println!("avg_derivative[{i}]: {avg_derivative}, delta orientation[{i}]: {delta_orientation}, last_gyro_data_halved[{i}]: {}", self.last_gyro_data_halved[i]);
                 self.orientation[i] = self.orientation[i] + delta_orientation;
+                self.fused_orientation[i] = self.fused_orientation[i] + delta_orientation;
                 self.last_gyro_data_halved[i] = new_halved_reading;
             }
             self.last_gyro_timestamp = timestamp;
         }
+        self.fused_orientation[0] = Self::complementary_filter(self.fused_orientation[0], self.accel_tilt[0]);
+        self.fused_orientation[1] = Self::complementary_filter(self.fused_orientation[1], self.accel_tilt[1]);
+        self.orientation = self.fused_orientation;
     }
 }
